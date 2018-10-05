f11831dec88f77218556dbdef5d34cb043c247b4
ThinhLuffy <41873503+ThinhLuffy@users.noreply.github.com>
2018-10-05 17:16:18 +0700

    Quan Ly Hoc Sinh

--- HocSinh/src/Main.java
+++ HocSinh/src/Main.java
@@ -1,8 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+
+        HocSinh studentManager = new HocSinh();
+        studentManager.displayAll();
+
         HocSinh andy = new HocSinh("Andy", 23);
-        System.out.println(andy.getInfomation());
-        System.out.println("name: " + andy.getName());
-        System.out.println("age: " + andy.getAge());
+        studentManager.add(andy);
+        System.out.println("added " + andy.getInfomation());
+        studentManager.displayAll();
+
+        HocSinh booby = new HocSinh("Booby", 25);
+        studentManager.add(booby);
+        System.out.println("added " + booby.getInfomation());
+        studentManager.displayAll();
+
+        HocSinh samy = new HocSinh("Samy", 22);
+        studentManager.add(samy);
+        System.out.println("added " + samy.getInfomation());
+        studentManager.displayAll();
     }
 }
