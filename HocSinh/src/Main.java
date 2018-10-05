461990d27f7f39ea2c1a91a5aa6d0817bbbced09
ThinhLuffy <41873503+ThinhLuffy@users.noreply.github.com>
2018-10-05 17:06:49 +0700

    Quan Ly Hoc Sinh

---
+++ HocSinh/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        HocSinh andy = new HocSinh("Andy", 23);
+        System.out.println(andy.getInfomation());
+        System.out.println("name: " + andy.getName());
+        System.out.println("age: " + andy.getAge());
+    }
+}
