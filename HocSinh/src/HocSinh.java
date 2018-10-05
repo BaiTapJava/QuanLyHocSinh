88ad4fae4cf473e91868ac404ac5f5d9d6b98be5
ThinhLuffy <41873503+ThinhLuffy@users.noreply.github.com>
2018-10-05 17:16:06 +0700

    Quan Ly Hoc Sinh

--- HocSinh/src/HocSinh.java
+++ HocSinh/src/HocSinh.java
@@ -1,8 +1,15 @@
 public class HocSinh {
-    private String name;
-    private int age;
+
+    private HocSinh[] students = new HocSinh[20];
+    private int size;
     public HocSinh() {
     }
+    public void add(HocSinh student) {
+        students[size] = student;
+        size++;
+    }
+    private String name;
+    private int age;
     public HocSinh(String name, int age) {
         this.name = name;
         this.age = age;
@@ -24,5 +31,38 @@ public class HocSinh {
                 "name='" + name + '\'' +
                 ", age=" + age +
                 '}';
+    }
+    public void displayAll() {
+        if (size == 0) {
+            System.out.println("There is no any student in list");
+        } else if (size == 1) {
+            System.out.println("There is 1 student in list");
+            System.out.print(students[0].getInfomation() + "");
+            System.out.println();
+        } else {
+            System.out.println("There are " + size + " students list: ");
+            for (int i = 0; i < size; i++) {
+                System.out.print(students[i].getInfomation() + ", ");
+            }
+            System.out.println();
+        }
+    }
+    public void find(String name) {
+        int found = 0;
+        for (int i = 0; i < size; i++) {
+            boolean match = students[i].getName().equals(name);
+            if (match) {
+                System.out.println("The position of student \""
+                        + name
+                        + "\" in the list is "
+                        + (i + 1));
+                found++;
+                break;
+            }
+        }
+        boolean notFound = found == 0;
+        if (notFound) {
+            System.out.println("There is no student with name " + name + " in the list");
+        }
     }
 }
